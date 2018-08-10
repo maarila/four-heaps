96a8e8a451b5dd82442cf8486cd639eba40a8e52
Mika Äärilä <maarila@gmail.com>
2018-08-10 20:09:26 +0300

    Add JavaDoc documentation for binomial heap

--- heaps/src/main/java/components/BinomialHeap.java
+++ heaps/src/main/java/components/BinomialHeap.java
@@ -1,21 +1,52 @@
 package components;
 
+/**
+ * This class provides the basic implementation of a binomial maximum heap. It
+ * contains the typical methods expected from a maximum heap - insertion,
+ * returning the maximum value, deleting maximum and increasing key.
+ *
+ * @author Mika Äärilä
+ */
 public class BinomialHeap {
 
+    /**
+     * The basic element of a binomial heap - a head node that provides links to
+     * its parent, child and sibling. In addition, head maintains its key value
+     * and its degree.
+     */
     private Node head;
 
+    /**
+     * Constructor for the binomial heap. At the start, the head is set to null.
+     */
     public BinomialHeap() {
         this.head = null;
     }
 
+    /**
+     * Creates and returns a new binomial heap with head set to null.
+     *
+     * @return a new binomial heap.
+     */
     public BinomialHeap makeBinomialHeap() {
         return new BinomialHeap();
     }
-    
+
+    /**
+     * Returns the head node of the current binomial heap.
+     *
+     * @return the head node.
+     */
     public Node getHead() {
         return head;
     }
 
+    /**
+     * Returns the maximum value of the heap.
+     *
+     * @return the maximum value found after iterating through the current head
+     * and its siblings i.e. the root list of the heap.
+     */
     public int returnMax() {
         Node iteratingHead = this.head;
         int maxValue = Integer.MIN_VALUE;
@@ -24,13 +55,20 @@ public class BinomialHeap {
             if (iteratingHead.getKey() > maxValue) {
                 maxValue = iteratingHead.getKey();
             }
-            
+
             iteratingHead = iteratingHead.getSiblingToTheRight();
         }
 
         return maxValue;
     }
 
+    /**
+     * Finds the maximum value of the heap, deletes it and then returns it.
+     * Before returning the maximum value, the method also reorganises the heap
+     * should it prove necessary.
+     *
+     * @return the maximum value that was just deleted.
+     */
     public int deleteMax() {
         if (this.head == null) {
             return Integer.MIN_VALUE;
@@ -74,11 +112,17 @@ public class BinomialHeap {
         return maxValue;
     }
 
+    /**
+     * Increases the value of the key in the given node.
+     *
+     * @param nodeToIncrease the node containing the key to be increased.
+     * @param newKey the new value of the key.
+     */
     public void increaseKey(Node nodeToIncrease, int newKey) {
         if (newKey > nodeToIncrease.getKey()) {
             nodeToIncrease.setKey(newKey);
             Node iteratingNode = nodeToIncrease;
-            
+
             while (iteratingNode.getParent() != null
                     && iteratingNode.getParent().getKey() > iteratingNode.getKey()) {
                 int helperValue = iteratingNode.getKey();
@@ -89,6 +133,12 @@ public class BinomialHeap {
         }
     }
 
+    /**
+     * Inserts a new value into the heap. Insertion requires the heap to be
+     * rebuilt with the union method.
+     *
+     * @param newValue the value to be inserted into the heap.
+     */
     public void insert(int newValue) {
         Node newNode = new Node(newValue);
         BinomialHeap newHeap = makeBinomialHeap();
@@ -97,13 +147,14 @@ public class BinomialHeap {
         this.head = unitedHeap.head;
     }
 
-    public void link(Node firstNode, Node secondNode) {
-        firstNode.setParent(secondNode);
-        firstNode.setSiblingToTheRight(secondNode.getLeftmostChild());
-        secondNode.setLeftmostChild(firstNode);
-        secondNode.setDegree(secondNode.getDegree() + 1);
-    }
-
+    /**
+     * Unites two binomial heaps into one while making sure that the resulting
+     * heap abides by the binomial heap properties.
+     *
+     * @param secondHeap the heap to be united with the current heap.
+     * @return the heap that resulted from uniting the current heap with the
+     * given heap.
+     */
     public BinomialHeap union(BinomialHeap secondHeap) {
         BinomialHeap unitedHeap = makeBinomialHeap();
         unitedHeap.head = merge(this, secondHeap);
@@ -145,6 +196,29 @@ public class BinomialHeap {
         return unitedHeap;
     }
 
+    /**
+     * Helper method for the union operation. Its purpose is to make the given
+     * first node the new head of the second node's child nodes.
+     *
+     * @param firstNode the first node to be linked.
+     * @param secondNode the second node to be linked.
+     */
+    public void link(Node firstNode, Node secondNode) {
+        firstNode.setParent(secondNode);
+        firstNode.setSiblingToTheRight(secondNode.getLeftmostChild());
+        secondNode.setLeftmostChild(firstNode);
+        secondNode.setDegree(secondNode.getDegree() + 1);
+    }
+
+    /**
+     * Helper method for the union operation. Its purpose is to merge the root
+     * lists of the given heaps into a single linked list which is sorted by
+     * degree into increasing order.
+     *
+     * @param firstHeap the first heap to be merged.
+     * @param secondHeap the second heap to be merged.
+     * @return the merged heap.
+     */
     public Node merge(BinomialHeap firstHeap, BinomialHeap secondHeap) {
         if (firstHeap.head == null) {
             return secondHeap.head;
--- heaps/src/main/java/components/Node.java
+++ heaps/src/main/java/components/Node.java
@@ -1,58 +1,125 @@
-
 package components;
 
+/**
+ * This class provides the node needed for the proper functioning of the
+ * binomial heap.
+ *
+ * @author Mika Äärilä
+ */
 public class Node {
+
+    /**
+     * The properties of node include links to its parent node, child node and
+     * sibling node. In addition, it holds the information regarding its key
+     * value and its degree. The maximum degree of any node in an n-node tree is
+     * log n.
+     */
     private int key;
     private Node parent;
     private Node leftmostChild;
     private Node siblingToTheRight;
     private int degree;
-    
-    public Node (int key) {
+
+    /**
+     * Constructor for the node.
+     *
+     * @param key the value of node's key property.
+     */
+    public Node(int key) {
         this.key = key;
         this.parent = null;
         this.leftmostChild = null;
         this.siblingToTheRight = null;
-        this.degree = 0;        
+        this.degree = 0;
     }
 
+    /**
+     * Returns the key value of the node.
+     *
+     * @return the value of the node's key property.
+     */
     public int getKey() {
         return key;
     }
 
+    /**
+     * Sets the key value of the node.
+     *
+     * @param key the new key of type integer.
+     */
     public void setKey(int key) {
         this.key = key;
     }
 
+    /**
+     * Returns the parent node of the current node.
+     *
+     * @return the parent node.
+     */
     public Node getParent() {
         return parent;
     }
 
+    /**
+     * Sets the parent node of the current node.
+     *
+     * @param parent the new parent node.
+     */
     public void setParent(Node parent) {
         this.parent = parent;
     }
 
+    /**
+     * Returns the child node for the current node.
+     *
+     * @return the child node.
+     */
     public Node getLeftmostChild() {
         return leftmostChild;
     }
 
+    /**
+     * Sets the child node for the current node.
+     *
+     * @param leftmostChild the new child node.
+     */
     public void setLeftmostChild(Node leftmostChild) {
         this.leftmostChild = leftmostChild;
     }
 
+    /**
+     * Returns the sibling node of the current node.
+     *
+     * @return the sibling node.
+     */
     public Node getSiblingToTheRight() {
         return siblingToTheRight;
     }
 
+    /**
+     * Sets the sibling node of the current node.
+     *
+     * @param siblingToTheRight the new sibling node.
+     */
     public void setSiblingToTheRight(Node siblingToTheRight) {
         this.siblingToTheRight = siblingToTheRight;
     }
 
+    /**
+     * Returns the degree of the current node.
+     *
+     * @return the current degree of type integer.
+     */
     public int getDegree() {
         return degree;
     }
 
+    /**
+     * Sets the degree of the current node.
+     *
+     * @param degree the new degree of type integer.
+     */
     public void setDegree(int degree) {
         this.degree = degree;
-    }   
+    }
 }
