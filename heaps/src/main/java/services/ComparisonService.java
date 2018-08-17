60461b9a66d4497770450cc1a73e0db362a308f9
Mika Äärilä <maarila@gmail.com>
2018-08-17 18:35:12 +0300

    Add preliminary performance tests for insert

--- heaps/src/main/java/services/ComparisonService.java
+++ heaps/src/main/java/services/ComparisonService.java
@@ -1,6 +1,50 @@
-
 package services;
+
+import components.BinaryHeap;
+import components.BinomialHeap;
 
 public class ComparisonService {
 
+    public ComparisonService() {
+    }
+
+    public void runBinaryHeapInsert() {
+        BinaryHeap heap = new BinaryHeap(1);
+        long timeAtStart;
+        long timeAtEnd;
+        long average = 0;
+
+        for (int i = 0; i < 10; i++) {
+            timeAtStart = System.currentTimeMillis();
+            
+            for (int j = 0; j < 1000000; j++) {
+                heap.insert(j);
+            }
+            
+            timeAtEnd = System.currentTimeMillis();
+            average += (timeAtEnd - timeAtStart);
+        }
+
+        System.out.println("Binary heap insert: average " + average / 10 + " milliseconds.");
+    }
+    
+    public void runBinomialHeapInsert() {
+        BinomialHeap heap = new BinomialHeap();
+        long timeAtStart;
+        long timeAtEnd;
+        long average = 0;
+
+        for (int i = 0; i < 10; i++) {
+            timeAtStart = System.currentTimeMillis();
+            
+            for (int j = 0; j < 1000000; j++) {
+                heap.insert(j);
+            }
+            
+            timeAtEnd = System.currentTimeMillis();
+            average += (timeAtEnd - timeAtStart);
+        }
+
+        System.out.println("Binomial heap insert: average " + average / 10 + " milliseconds.");
+    }
 }
--- heaps/src/main/java/ui/ComparisonRunner.java
+++ heaps/src/main/java/ui/ComparisonRunner.java
@@ -1,4 +1,3 @@
-
 package ui;
 
 import services.ComparisonService;
@@ -7,48 +6,51 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class ComparisonRunner {
-    
+
     private final Scanner inputReader;
     private final ComparisonService comparisonService;
     private Map<String, String> commands;
-    
+
     public ComparisonRunner(Scanner inputReader, ComparisonService comparisonService) {
         this.inputReader = inputReader;
         this.comparisonService = comparisonService;
-        
+
         commands = new TreeMap<>();
-        
-        commands.put("1", "Run timed tests for binary heap's insert operation");
-        commands.put("2", "More to come...");
+
+        commands.put("1", "Run timed tests for insert operation");
+        commands.put("2", "Run timed tests for binary heap's insert operation");
+        commands.put("3", "Run timed tests for binomial heap's insert operation");
         commands.put("q", "Quit");
     }
-    
+
     public void start() {
         System.out.println();
         System.out.println("Welcome to comparing four heaps!");
         System.out.println();
         showOptions();
-        
-        commandLoop: while (true) {
+
+        commandLoop:
+        while (true) {
             System.out.println();
             System.out.println("Enter command: ");
             String command = inputReader.nextLine();
-            
+
             if (!commands.keySet().contains(command)) {
                 System.out.println();
                 System.out.println("Unknown command!");
                 System.out.println();
                 showOptions();
             }
-            
+
             switch (command) {
                 case "1":
-                    runBinaryHeapInsert();
+                    runInsertTests();
                     break;
                 case "2":
-                    System.out.println();
-                    System.out.println("Nothing too see here.");
-                    System.out.println();
+                    runBinaryHeapInsertTests();
+                    break;
+                case "3":
+                    runBinomialHeapInsertTests();
                     break;
                 case "q":
                     break commandLoop;
@@ -61,9 +63,32 @@ public class ComparisonRunner {
 
     }
     
-    public void runBinaryHeapInsert() {
+    public void runInsertTests() {
         System.out.println();
-        System.out.println("Hello heaps!");
+        System.out.println("Running insert tests...");
+        System.out.println();
+        comparisonService.runBinaryHeapInsert();
+        comparisonService.runBinomialHeapInsert();
+        System.out.println();
+        System.out.println("Tests completed!");
     }
-    
+
+    public void runBinaryHeapInsertTests() {
+        System.out.println();
+        System.out.println("Running binary heap insert tests...");
+        System.out.println();
+        comparisonService.runBinaryHeapInsert();
+        System.out.println();
+        System.out.println("Tests completed!");
+    }
+
+    public void runBinomialHeapInsertTests() {
+        System.out.println();
+        System.out.println("Running binomial heap insert tests...");
+        System.out.println();
+        comparisonService.runBinomialHeapInsert();
+        System.out.println();
+        System.out.println("Tests completed!");
+    }
+
 }
