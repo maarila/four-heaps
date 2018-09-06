818ebd68c94a2c34af778c8482bbfc56cb64f293
Mika Äärilä <maarila@gmail.com>
2018-09-06 08:12:58 +0300

    Refactor UI and performance tests

--- heaps/src/main/java/services/ComparisonService.java
+++ heaps/src/main/java/services/ComparisonService.java
@@ -3,90 +3,236 @@ package services;
 import components.BinaryHeap;
 import components.BinomialHeap;
 import components.FibonacciHeap;
+import components.Heap;
 import components.PairingHeap;
+import java.util.PriorityQueue;
 
 public class ComparisonService {
 
+    long timesEachTestRun;
+    long timeAtStart;
+    long timeAtEnd;
+    long totalTime;
+
     public ComparisonService() {
+        this.timesEachTestRun = 10;
     }
 
-    public void runBinaryHeapInsert() {
-        BinaryHeap heap = new BinaryHeap(1);
-        long timeAtStart;
-        long timeAtEnd;
-        long average = 0;
-
-        for (int i = 0; i < 10; i++) {
-            timeAtStart = System.currentTimeMillis();
-            
-            for (int j = 0; j < 1000000; j++) {
-                heap.insert(j);
-            }
-            
-            timeAtEnd = System.currentTimeMillis();
-            average += (timeAtEnd - timeAtStart);
-        }
-
-        System.out.println("Binary heap insert: average " + average / 10 + " milliseconds.");
+    public void runInsertTests(int numberOfTimes) {
+        binaryHeapInsert(numberOfTimes);
+        binomialHeapInsert(numberOfTimes);
+        pairingHeapInsert(numberOfTimes);
+        fibonacciHeapInsert(numberOfTimes);
+        priorityQueueInsert(numberOfTimes);
     }
 
-    public void runBinomialHeapInsert() {
-        BinomialHeap heap = new BinomialHeap();
-        long timeAtStart;
-        long timeAtEnd;
-        long average = 0;
-
-        for (int i = 0; i < 10; i++) {
-            timeAtStart = System.currentTimeMillis();
-
-            for (int j = 0; j < 1000000; j++) {
-                heap.insert(j);
-            }
-
-            timeAtEnd = System.currentTimeMillis();
-            average += (timeAtEnd - timeAtStart);
-        }
-
-        System.out.println("Binomial heap insert: average " + average / 10 + " milliseconds.");
+    public void runReturnMaxTests(int numberOfTimes) {
+        binaryHeapReturnMax(numberOfTimes);
+        binomialHeapReturnMax(numberOfTimes);
+        pairingHeapReturnMax(numberOfTimes);
+        fibonacciHeapReturnMax(numberOfTimes);
+        priorityQueueReturnMax(numberOfTimes);
     }
 
-    public void runPairingHeapInsert() {
-        PairingHeap heap = new PairingHeap();
-        long timeAtStart;
-        long timeAtEnd;
-        long average = 0;
-
-        for (int i = 0; i < 10; i++) {
-            timeAtStart = System.currentTimeMillis();
-
-            for (int j = 0; j < 1000000; j++) {
-                heap.insert(j);
-            }
-
-            timeAtEnd = System.currentTimeMillis();
-            average += (timeAtEnd - timeAtStart);
-        }
-
-        System.out.println("Pairing heap insert: average " + average / 10 + " milliseconds.");
+    public void runDeleteMaxTests(int numberOfTimes) {
+        binaryHeapDeleteMax(numberOfTimes);
+        binomialHeapDeleteMax(numberOfTimes);
+        pairingHeapDeleteMax(numberOfTimes);
+        fibonacciHeapDeleteMax(numberOfTimes);
+        priorityQueueDeleteMax(numberOfTimes);
     }
 
-    public void runFibonacciHeapInsert() {
-        FibonacciHeap heap = new FibonacciHeap();
-        long timeAtStart;
-        long timeAtEnd;
-        long average = 0;
+    public void binaryHeapInsert(int numberOfTimes) {
+        BinaryHeap binaryHeap = new BinaryHeap(numberOfTimes);
+        long average = insert(binaryHeap, numberOfTimes);
+        System.out.println("Binary heap insert: average " + average + " milliseconds.");
+    }
 
-        for (int i = 0; i < 10; i++) {
+    public void binomialHeapInsert(int numberOfTimes) {
+        BinomialHeap binomialHeap = new BinomialHeap();
+        long average = insert(binomialHeap, numberOfTimes);
+        System.out.println("Binomial heap insert: average " + average + " milliseconds.");
+    }
+
+    public void pairingHeapInsert(int numberOfTimes) {
+        PairingHeap pairingHeap = new PairingHeap();
+        long average = insert(pairingHeap, numberOfTimes);
+        System.out.println("Pairing heap insert: average " + average + " milliseconds.");
+    }
+
+    public void fibonacciHeapInsert(int numberOfTimes) {
+        FibonacciHeap fibonacciHeap = new FibonacciHeap();
+        long average = insert(fibonacciHeap, numberOfTimes);
+        System.out.println("Fibonacci heap insert: average " + average + " milliseconds.");
+    }
+
+    public void binaryHeapReturnMax(int numberOfTimes) {
+        BinaryHeap binaryHeap = new BinaryHeap(numberOfTimes);
+        long average = returnMax(binaryHeap, numberOfTimes);
+        System.out.println("Binary heap return-max: average " + average + " milliseconds.");
+    }
+
+    public void binomialHeapReturnMax(int numberOfTimes) {
+        BinomialHeap binomialHeap = new BinomialHeap();
+        long average = returnMax(binomialHeap, numberOfTimes);
+        System.out.println("Binomial heap return-max: average " + average + " milliseconds.");
+    }
+
+    public void pairingHeapReturnMax(int numberOfTimes) {
+        PairingHeap pairingHeap = new PairingHeap();
+        long average = returnMax(pairingHeap, numberOfTimes);
+        System.out.println("Pairing heap return-max: average " + average + " milliseconds.");
+    }
+
+    public void fibonacciHeapReturnMax(int numberOfTimes) {
+        FibonacciHeap fibonacciHeap = new FibonacciHeap();
+        long average = returnMax(fibonacciHeap, numberOfTimes);
+        System.out.println("Fibonacci heap return-max: average " + average + " milliseconds.");
+    }
+
+    public void binaryHeapDeleteMax(int numberOfTimes) {
+        BinaryHeap binaryHeap = new BinaryHeap(numberOfTimes + 1000);
+        long average = deleteMax(binaryHeap, numberOfTimes);
+        System.out.println("Binary heap delete-max: average " + average + " milliseconds.");
+    }
+
+    public void binomialHeapDeleteMax(int numberOfTimes) {
+        BinomialHeap binomialHeap = new BinomialHeap();
+        long average = deleteMax(binomialHeap, numberOfTimes);
+        System.out.println("Binomial heap delete-max: average " + average + " milliseconds.");
+    }
+
+    public void pairingHeapDeleteMax(int numberOfTimes) {
+        PairingHeap pairingHeap = new PairingHeap();
+        long average = deleteMax(pairingHeap, numberOfTimes);
+        System.out.println("Pairing heap delete-max: average " + average + " milliseconds.");
+    }
+
+    public void fibonacciHeapDeleteMax(int numberOfTimes) {
+        FibonacciHeap fibonacciHeap = new FibonacciHeap();
+        long average = deleteMax(fibonacciHeap, numberOfTimes);
+        System.out.println("Fibonacci heap delete-max: average " + average + " milliseconds.");
+    }
+
+    public long insert(Heap heap, int numberOfTimesRun) {
+        totalTime = 0;
+
+        for (int i = 0; i < timesEachTestRun; i++) {
             timeAtStart = System.currentTimeMillis();
 
-            for (int j = 0; j < 1000000; j++) {
+            for (int j = 0; j < numberOfTimesRun; j++) {
                 heap.insert(j);
             }
 
             timeAtEnd = System.currentTimeMillis();
-            average += (timeAtEnd - timeAtStart);
+            totalTime += (timeAtEnd - timeAtStart);
         }
 
-        System.out.println("Fibonacci heap insert: average " + average / 10 + " milliseconds.");
+        return totalTime / timesEachTestRun;
+    }
+
+    public void priorityQueueInsert(int numberOfTimesRun) {
+        PriorityQueue queue = new PriorityQueue();
+        totalTime = 0;
+
+        for (int i = 0; i < timesEachTestRun; i++) {
+            timeAtStart = System.currentTimeMillis();
+
+            for (int j = 0; j < numberOfTimesRun; j++) {
+                queue.add(j);
+            }
+
+            timeAtEnd = System.currentTimeMillis();
+            totalTime += (timeAtEnd - timeAtStart);
+        }
+
+        System.out.println("Priority queue insert: average " + totalTime / timesEachTestRun + " milliseconds.");
+    }
+
+    public long returnMax(Heap heap, int numberOfTimesRun) {
+        totalTime = 0;
+
+        for (int i = 0; i < numberOfTimesRun; i++) {
+            heap.insert(i);
+        }
+
+        for (int i = 0; i < timesEachTestRun; i++) {
+            timeAtStart = System.currentTimeMillis();
+
+            for (int j = 0; j < numberOfTimesRun; j++) {
+                heap.returnMax();
+            }
+
+            timeAtEnd = System.currentTimeMillis();
+            totalTime += (timeAtEnd - timeAtStart);
+        }
+
+        return totalTime / timesEachTestRun;
+    }
+
+    public void priorityQueueReturnMax(int numberOfTimesRun) {
+        PriorityQueue queue = new PriorityQueue();
+        totalTime = 0;
+
+        for (int i = 0; i < numberOfTimesRun; i++) {
+            queue.add(i);
+        }
+
+        for (int i = 0; i < timesEachTestRun; i++) {
+            timeAtStart = System.currentTimeMillis();
+
+            for (int j = 0; j < numberOfTimesRun; j++) {
+                queue.peek();
+            }
+
+            timeAtEnd = System.currentTimeMillis();
+            totalTime += (timeAtEnd - timeAtStart);
+        }
+
+        System.out.println("Priority queue return-max: average " + totalTime / timesEachTestRun + " milliseconds.");
+    }
+
+    public long deleteMax(Heap heap, int numberOfTimesRun) {
+        totalTime = 0;
+
+        for (int i = 0; i < timesEachTestRun; i++) {
+
+            for (int j = 0; j < numberOfTimesRun; j++) {
+                heap.insert(i);
+            }
+
+            timeAtStart = System.currentTimeMillis();
+
+            for (int k = 0; k < numberOfTimesRun; k++) {
+                heap.deleteMax();
+            }
+
+            timeAtEnd = System.currentTimeMillis();
+            totalTime += (timeAtEnd - timeAtStart);
+        }
+
+        return totalTime / timesEachTestRun;
+    }
+
+    public void priorityQueueDeleteMax(int numberOfTimesRun) {
+        PriorityQueue queue = new PriorityQueue();
+        totalTime = 0;
+
+        for (int i = 0; i < numberOfTimesRun; i++) {
+            queue.add(i);
+        }
+
+        for (int i = 0; i < timesEachTestRun; i++) {
+            timeAtStart = System.currentTimeMillis();
+
+            for (int j = 0; j < numberOfTimesRun; j++) {
+                queue.poll();
+            }
+
+            timeAtEnd = System.currentTimeMillis();
+            totalTime += (timeAtEnd - timeAtStart);
+        }
+
+        System.out.println("Priority queue delete-max: average " + totalTime / timesEachTestRun + " milliseconds.");
     }
 }
--- heaps/src/main/java/ui/ComparisonRunner.java
+++ heaps/src/main/java/ui/ComparisonRunner.java
@@ -9,19 +9,20 @@ public class ComparisonRunner {
 
     private final Scanner inputReader;
     private final ComparisonService comparisonService;
+    private int numberOfTimes;
     private Map<String, String> commands;
 
     public ComparisonRunner(Scanner inputReader, ComparisonService comparisonService) {
         this.inputReader = inputReader;
         this.comparisonService = comparisonService;
+        this.numberOfTimes = 100000;
 
         commands = new TreeMap<>();
 
-        commands.put("1", "Run timed tests for insert operation");
-        commands.put("2", "Run timed tests for binary heap's insert operation");
-        commands.put("3", "Run timed tests for binomial heap's insert operation");
-        commands.put("4", "Run timed tests for pairing heap's insert operation");
-        commands.put("5", "Run timed tests for fibonacci heap's insert operation");
+        commands.put("1", "Run performance tests for insert");
+        commands.put("2", "Run performance tests for return-max");
+        commands.put("3", "Run performance tests for delete-max");
+        commands.put("4", "Run all performance tests");
         commands.put("q", "Quit");
     }
 
@@ -30,37 +31,71 @@ public class ComparisonRunner {
         System.out.println("Welcome to comparing four heaps!");
         System.out.println();
 
-        commandLoop:
         while (true) {
             showOptions();
             System.out.println();
             System.out.println("Enter command: ");
             String command = inputReader.nextLine();
 
+            if (command.equals("q")) {
+                break;
+            }
+
             if (!commands.keySet().contains(command)) {
                 System.out.println();
                 System.out.println("Unknown command!");
                 System.out.println();
+                continue;
+            }
+
+            try {
+                System.out.println("Please enter the number of operations to run:");
+                numberOfTimes = Integer.parseInt(inputReader.nextLine());
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println();
+                System.out.println("Please enter a valid number.");
+                System.out.println();
+                continue;
             }
 
             switch (command) {
                 case "1":
-                    runInsertTests();
+                    System.out.println("Running insert tests...");
+                    System.out.println();
+                    runInsertTests(numberOfTimes);
+                    System.out.println();
+                    System.out.println("Tests completed!");
+                    System.out.println();
                     break;
                 case "2":
-                    runBinaryHeapInsertTests();
+                    System.out.println("Running return-max tests...");
+                    System.out.println();
+                    runReturnMaxTests(numberOfTimes);
+                    System.out.println();
+                    System.out.println("Tests completed!");
+                    System.out.println();
                     break;
                 case "3":
-                    runBinomialHeapInsertTests();
+                    System.out.println("Running delete-max tests...");
+                    System.out.println();
+                    runDeleteMaxTests(numberOfTimes);
+                    System.out.println();
+                    System.out.println("Tests completed!");
+                    System.out.println();
                     break;
                 case "4":
-                    runPairingHeapInsertTests();
+                    System.out.println("Running all tests...");
+                    System.out.println();
+                    runInsertTests(numberOfTimes);
+                    System.out.println();
+                    runReturnMaxTests(numberOfTimes);
+                    System.out.println();
+                    runDeleteMaxTests(numberOfTimes);
+                    System.out.println();
+                    System.out.println("Tests completed!");
+                    System.out.println();
                     break;
-                case "5":
-                    runFibonacciHeapInsertTests();
-                    break;
-                case "q":
-                    break commandLoop;
             }
         }
     }
@@ -70,51 +105,15 @@ public class ComparisonRunner {
 
     }
 
-    public void runInsertTests() {
-        System.out.println();
-        System.out.println("Running insert tests...");
-        System.out.println();
-        comparisonService.runBinaryHeapInsert();
-        comparisonService.runBinomialHeapInsert();
-        comparisonService.runPairingHeapInsert();
-        comparisonService.runFibonacciHeapInsert();
-        System.out.println();
-        System.out.println("Tests completed!");
+    public void runInsertTests(int numberOfTimes) {
+        comparisonService.runInsertTests(numberOfTimes);
     }
 
-    public void runBinaryHeapInsertTests() {
-        System.out.println();
-        System.out.println("Running binary heap insert tests...");
-        System.out.println();
-        comparisonService.runBinaryHeapInsert();
-        System.out.println();
-        System.out.println("Tests completed!");
+    public void runReturnMaxTests(int numberOfTimes) {
+        comparisonService.runReturnMaxTests(numberOfTimes);
     }
 
-    public void runBinomialHeapInsertTests() {
-        System.out.println();
-        System.out.println("Running binomial heap insert tests...");
-        System.out.println();
-        comparisonService.runBinomialHeapInsert();
-        System.out.println();
-        System.out.println("Tests completed!");
-    }
-
-    public void runPairingHeapInsertTests() {
-        System.out.println();
-        System.out.println("Running pairing heap insert tests...");
-        System.out.println();
-        comparisonService.runPairingHeapInsert();
-        System.out.println();
-        System.out.println("Tests completed!");
-    }
-
-    public void runFibonacciHeapInsertTests() {
-        System.out.println();
-        System.out.println("Running fibonacci heap insert tests...");
-        System.out.println();
-        comparisonService.runFibonacciHeapInsert();
-        System.out.println();
-        System.out.println("Tests completed!");
+    public void runDeleteMaxTests(int numberOfTimes) {
+        comparisonService.runDeleteMaxTests(numberOfTimes);
     }
 }
